cbba67f18ad75342cc743d783f59146d0820db96
Eliseo Guevara <gerardoeliseo20@gmail.com>
2026-03-17 15:29:48 -0600

    ajuste

--- Lab1_C2/vite.config.js
+++ Lab1_C2/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'Lab1_C2',
+  base:'/Lab1_C2',
   plugins: [
     vue(),
     vueDevTools(),
